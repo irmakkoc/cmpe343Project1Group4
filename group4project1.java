c99b1dd79914fde23b0935fea6fb113964136ddd
irmakkoc <99953172+irmakkoc@users.noreply.github.com>
2024-10-23 17:09:39 +0300

    Update group4project1.java

--- group4project1.java
+++ group4project1.java
@@ -51,6 +51,151 @@ public static void statisticalInformation() {
 
     public static void matrixOperations() {
         System.out.println("Matrix Operations.");
+        
+        boolean inSubmenu = true;
+        
+        Scanner scan = new Scanner(System.in);
+        
+        while(inSubmenu) {
+        	System.out.println("Please select an operation");
+        	System.out.println("Enter 'A' for Transpose");
+        	System.out.println("Enter 'B' for Inverse");
+        	System.out.println("Enter 'C' for Matrix Multiplication");
+        	System.out.println("Enter 'D' for Element-Wise Matrix Multiplication");
+        	System.out.println("Press 'E' to return to the Main Menu");
+        
+        	String submenu = scan.nextLine().toUpperCase();
+        	switch (submenu.toUpperCase()){
+        		case "A":
+        			//Transpose();
+        			break;
+        		case "B":
+        			//Inverse();
+        			break;
+        		case "C":
+        			//MatrixMultiplication();
+        			break;
+        		case "D":
+        			ElementWiseMatrixMultip();
+        			break;
+        		case "E":
+        			inSubmenu = false;
+        			break;
+        		default:
+        			System.out.println("Invalid option. Please select a valid operation.");
+        			break;
+        	}	
+        }
+}
+		
+		public static void ElementWiseMatrixMultip() {
+		Scanner input = new Scanner(System.in);
+		
+        System.out.println("Enter a positive integer row number for your matrices");
+        int rows1 = -1; 
+        while (rows1 <= 0) { 
+            try {
+                System.out.println("Enter a positive integer row number for your matrices:");
+                rows1 = input.nextInt(); 
+                if (rows1 <= 0) {
+                    System.out.println("You must enter a positive integer. Try again.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. You must enter a number. Try again.");
+                input.next(); 
+            }
+        }
+        int rows2 = rows1;
+        int rows3 = rows1;
+        
+        System.out.println("Please enter a positive integer column number for your matrices");
+        int cols1 = -1; 
+        while (cols1 <= 0) { 
+            try {
+                System.out.println("Please enter a positive integer column number for your matrices:");
+                cols1 = input.nextInt(); 
+                if (cols1 <= 0) {
+                    System.out.println("You must enter a positive integer. Try again.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. You must enter a number. Try again.");
+                input.next(); 
+            }
+        }
+
+        int cols2 = cols1; 
+        int cols3 = cols1; 
+        
+        
+        
+        double[][] matrix1 = new double[rows1][cols1];
+        double[][] matrix2 = new double[rows2][cols2];
+        double[][] resultMatrix = new double [rows3][cols3];
+        
+        System.out.println("Please enter the elements for your first matrix");
+        for(int i=0; i<rows1; i++) {
+        	for(int j=0; j<cols1; j++) {
+        		while(true) {
+        			try {
+        				matrix1[i][j] = input.nextDouble();
+        				break;
+        			} catch(InputMismatchException e) {
+        				System.out.println("You must enter a double number. Try again. ");
+        				input.next();
+        			}
+        		}
+        	}
+        		
+        	
+        }
+        System.out.println("Your first matrix is: ");
+        for(int i=0; i<rows1; i++) {
+        	for(int j=0; j<cols1; j++) {
+        		System.out.print(matrix1[i][j] + " ");
+        	}
+        	System.out.println();
+        	
+        }
+        
+        System.out.println("Please enter the elements for your second matrix");
+        for(int i=0; i<rows2; i++) {
+        	for(int j=0; j<cols2; j++) {
+        		while(true) {
+        			try {
+        				matrix2[i][j] = input.nextDouble();
+        				break;
+        			} catch(InputMismatchException e) {
+        				System.out.println("You must enter a double number. Try again. ");
+        				input.next();
+        			}
+        		}
+        	}
+        }
+        
+        System.out.println("Your second matrix is: ");
+        for(int i=0; i<rows2; i++) {
+        	for(int j=0; j<cols2; j++) {
+        		
+        		System.out.print(matrix2[i][j] + " ");
+        	}
+        	System.out.println();
+        	
+        }
+        
+        for (int i = 0; i < rows3; i++) {
+            for (int j = 0; j < cols3; j++) {
+                resultMatrix[i][j] = matrix1[i][j] * matrix2[i][j];
+            }
+        }
+
+        System.out.println("The result of element-wise matrix multiplication is:");
+        for (int i = 0; i < rows3; i++) {
+            for (int j = 0; j < cols3; j++) {
+                System.out.print(resultMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+           
     }
 
     public static void textEncryptionDecryption() {
